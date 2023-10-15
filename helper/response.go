64f2f0517095c1bd5ea2c747abf8e7f5d3b54e73
thoriqadillah <thoriqadillah59@gmail.com>
2023-10-15 08:34:26 +0700

    change response message

--- helper/response.go
+++ helper/response.go
@@ -2,32 +2,30 @@ package helper
 
 import "github.com/gofiber/fiber/v2"
 
-func Error(ctx *fiber.Ctx, message string) error {
-	return ctx.Status(fiber.StatusBadRequest).
+func Error(ctx *fiber.Ctx, message string, code ...int) error {
+	statusCode := fiber.StatusBadRequest
+	if len(code) > 0 {
+		statusCode = code[0]
+	}
+
+	return ctx.Status(statusCode).
 		JSON(fiber.Map{
-			"status":  "error",
 			"message": message,
 		})
 }
 
 func Success(ctx *fiber.Ctx, body interface{}) error {
-	return ctx.Status(fiber.StatusOK).
-		JSON(fiber.Map{
-			"status": "ok",
-			"data":   body,
-		})
+	return ctx.Status(fiber.StatusOK).JSON(body)
 }
 
 func Created(ctx *fiber.Ctx) error {
-	return ctx.Status(fiber.StatusCreated).
-		JSON(fiber.Map{
-			"status": "ok",
-		})
+	return ctx.SendStatus(fiber.StatusCreated)
 }
 
 func NotFound(ctx *fiber.Ctx) error {
-	return ctx.Status(fiber.StatusNotFound).
-		JSON(fiber.Map{
-			"status": "not found",
-		})
+	return Error(ctx, "Not Found", fiber.StatusNotFound)
+}
+
+func Unauthorized(ctx *fiber.Ctx) error {
+	return Error(ctx, "Unauthorized", fiber.StatusUnauthorized)
 }
