cd7e33c7b95334aa6f1107da4abeade9f2b214e8
thoriqadillah <thoriqadillah59@gmail.com>
2023-12-22 17:43:57 +0700

    update docs

--- entry/api/api.go
+++ entry/api/api.go
@@ -59,7 +59,7 @@ func (s *entryService) fetch(ctx *fiber.Ctx) error {
 	}
 
 	if err := s.store.Create(entry.ID(), toDownload); err != nil {
-		return response.Error(ctx, err.Error(), fiber.StatusInternalServerError)
+		return response.InternalServerError(ctx, err.Error())
 	}
 
 	return response.Success(ctx, toDownload)
@@ -150,7 +150,7 @@ func (s *entryService) Routes() []api.Route {
 			Handler: s.updateAllEntry,
 		},
 		{
-			Path:    "/delete/entries/:id",
+			Path:    "/entries/:id",
 			Method:  "DELETE",
 			Handler: s.deleteEntry,
 		},
