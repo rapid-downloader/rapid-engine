9d692e7cba35890e5928bb02b54932c0f23ff221
thoriqadillah <thoriqadillah59@gmail.com>
2024-02-17 14:14:43 +0700

    unused context menu

--- gui/frontend/src/module/home/components/DownloadList.vue
+++ gui/frontend/src/module/home/components/DownloadList.vue
@@ -17,6 +17,10 @@ import {
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 import { useRouteQuery } from '@vueuse/router';
 import { Button } from '@/components/ui/button';
+import Dialog from '@/components/ui/dialog/XDialog.vue';
+import { Actionable } from '@/components/ui/dialog';
+import Confirmation from '@/components/Confirmation.vue';
+import { Checkbox } from '@/components/ui/checkbox';
 
 const props = defineProps<{
     items: Record<string, Download>,
@@ -31,7 +35,8 @@ function sort(row: Sort) {
 }
 
 const emit = defineEmits<{
-    (e: 'paginate'): void
+    (e: 'paginate'): void,
+    (e: 'delete', id: string): void
 }>()
 
 const search = useRouteQuery('search', '')
@@ -102,6 +107,12 @@ onUnmounted(() => {
     document.removeEventListener('scroll', onScroll)
     document.removeEventListener('resize', onScroll)
 })
+
+const wantRemoveFromDisk = ref(false)
+function remove(id: string) {
+
+}
+
 </script>
 
 <template>
@@ -115,9 +126,7 @@ onUnmounted(() => {
                 <table-caption></table-caption>
                 <table-header>
                     <table-row class="hover:bg-secondary border-muted-foreground">
-                        <table-head class="w-[2rem]">
-                            Type
-                        </table-head>
+                        <table-head class="w-[2rem]">Type</table-head>
                         <table-head @click="sort('name')" class="cursor-pointer">
                             <div class="flex justify-between items-center">
                                 <p>Name</p>
@@ -130,18 +139,10 @@ onUnmounted(() => {
                                 <i-radix-icons-caret-sort />
                             </div>
                         </table-head>
-                        <table-head class="w-[8%]">
-                            Progress
-                        </table-head>
-                        <table-head class="min-w-[5rem]">
-                            Time Left
-                        </table-head>
-                        <table-head class="min-w-[7rem] lg:w-[10%]">
-                            Speed
-                        </table-head>
-                        <table-head class="w-[10%]">
-                            Status
-                        </table-head>
+                        <table-head class="w-[8%]">Progress</table-head>
+                        <table-head class="min-w-[5rem]">Time Left</table-head>
+                        <table-head class="min-w-[7rem] lg:w-[10%]">Speed</table-head>
+                        <table-head class="w-[10%]">Status</table-head>
                         <table-head @click="sort('date')" class="cursor-pointer min-w-[12rem]">
                             <div class="flex justify-between items-center">
                                 <p>Date</p>
@@ -159,37 +160,39 @@ onUnmounted(() => {
                             <p class="w-[95%] group-hover/cell:w-[75%] truncate">{{ item.name }}</p>
                             <div class="hidden group-hover/cell:flex absolute top-1/2 -translate-y-1/2 right-5 gap-2 py-2">
                                 <Button size="icon" variant="ghost" class="bg-muted group/action rounded-full hover:bg-secondary hover:text-foreground w-7 h-7">
-                                    <i-fluent:play-16-regular class="group-hover/action:hidden" />
-                                    <i-fluent:play-16-filled class="hidden group-hover/action:block" />
+                                    <i-fluent:pause-16-regular class="group-hover/action:hidden" />
+                                    <i-fluent:pause-16-filled class="hidden group-hover/action:block" />
                                 </Button>
                                 <Button size="icon" variant="ghost" class="group/action rounded-full text-warning hover:bg-warning hover:text-warning-foreground w-7 h-7">
                                     <i-fluent:stop-16-regular class="group-hover/action:hidden"/>
                                     <i-fluent:stop-16-filled class="hidden group-hover/action:block"/>
                                 </Button>
-                                <Button size="icon" variant="ghost" class="rounded-full text-destructive hover:bg-destructive hover:text-destructive-foreground w-7 h-7">
-                                    <i-fluent:delete-16-regular class="group-hover/action:hidden"/>
-                                    <i-fluent:delete-16-filled class="hidden group-hover/action:block"/>
-                                </Button>
+                                <Confirmation 
+                                    title="Delete download" 
+                                    :description="`This will delete ${item.name} from the list forever. If this is an on going download, it will be stopped and then deleted. Are you sure?`" 
+                                    :actionable="{ label: 'Delete', type: 'destructive', action: () => remove(item.id) }" >
+                                    <Button size="icon" variant="ghost" class="rounded-full text-destructive hover:bg-destructive hover:text-destructive-foreground w-7 h-7">
+                                        <i-fluent:delete-16-regular class="group-hover/action:hidden"/>
+                                        <i-fluent:delete-16-filled class="hidden group-hover/action:block"/>
+                                    </Button>
+
+                                    <template #additional>
+                                        <div class="w-full flex gap-2 items-center">
+                                            <Checkbox v-model="wantRemoveFromDisk" />
+                                            <p class="text-xs opacity-80">Remove from disk</p>
+                                        </div>
+                                    </template>
+                                </Confirmation>
                             </div>
                         </table-cell>
-                        <table-cell>
-                            {{ parseSize(item.size) }}
-                        </table-cell>
-                        <table-cell>
-                            {{ `${item.progress.toFixed(2)}%` }}
-                        </table-cell>
-                        <table-cell>
-                            {{ parseTimeleft(item.timeLeft) }}
-                        </table-cell>
-                        <table-cell>
-                            {{ `${parseSize(item.speed)}/s` }}
-                        </table-cell>
+                        <table-cell>{{ parseSize(item.size) }}</table-cell>
+                        <table-cell>{{ `${item.progress.toFixed(2)}%` }}</table-cell>
+                        <table-cell>{{ parseTimeleft(item.timeLeft) }}</table-cell>
+                        <table-cell>{{ `${parseSize(item.speed)}/s` }}</table-cell>
                         <table-cell :class="`font-medium flex gap-1 items-center ${statusColor(item.status)}`">
                             <status-icon :status="item.status" /> {{ item.status }}
                         </table-cell>
-                        <table-cell>
-                            {{ parseDate(item.date) }}
-                        </table-cell>
+                        <table-cell>{{ parseDate(item.date) }}</table-cell>
                     </table-row>
                 </table-body>
             </Table>
